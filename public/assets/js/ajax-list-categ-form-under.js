857c3b549c333d96d0366f0c76c1422438c1c96c
tatahappyness <florent.tata91@gmail.com>
2019-11-25 19:37:08 +0100

    update forms category and city

--- public/assets/js/ajax-list-categ-form-under.js
+++ public/assets/js/ajax-list-categ-form-under.js
@@ -8,38 +8,35 @@ jQuery.ajax({
     }).done(function(response){
 			
         //console.log(response);return false;
-        var input = document.getElementById("show-input-list-search-under-find");
-        autocomplete({
-            input: input,
-            minLength: 1,
-            emptyMsg: 'Aucun élément trouvé',
-            fetch: function(text, update) {
-                text = text.toLowerCase();
-                // you can also use AJAX requests instead of preloaded data
-                var suggestions = response.filter(n => n.label.toLowerCase().startsWith(text))
-                update(suggestions);
-            },
-            onSelect: function(item) {
-                input.value = item.label;
-                inputCreate2(item.value);
-            }
+        var options = {
 
-        });
-	            
+            data: response,
+            placeholder: "Quel travaux souhaitez vou realiser ?",
+            getValue: "label",
+
+            list: {
+		
+                match: {
+                enabled: true
+                },
+                onSelectItemEvent: function() {
+                    var value = $("#show-input-list-search-under-find").getSelectedItemData().value;
+                    //alert(value);
+                    jQuery("#metier_ask_devis").val(value).trigger("change");
+                    var form_dev = document.getElementById('form-ask-devis-pros-under');
+                    //submit the form post devis from home
+                    form_dev.submit();
+                }
+            },
+
+            theme: "square"
+        };
+        
+        jQuery("#show-input-list-search-under-find").easyAutocomplete(options);
+     
     }).fail(function(){
         // Here you should treat the http errors (e.g., 403, 40
         alert('serveur internal error!!')					
         }).always(function(){
             console.log("AJAX request finished!");
         });
-
-//Function to create element input
-function inputCreate2(id) {
-    var parentDiv = document.getElementById('form-hidden-under-find');
-    //console.log(parentDiv);
-    parentDiv.innerHTML = '<input type="hidden" name="metier_ask_devis" value="' + id +'">';
-    var form_dev = document.getElementById('form-ask-devis-pros-under');
-    //submit the form post devis from home
-    form_dev.submit();
-}
-//END AJAX FOR FORM UNDER IN SPACE FIND PROS
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -42,7 +42,7 @@ class RegistrationController extends AbstractController
         if ($request->request->get('TYPE_USER') !== null && $request->request->get('TYPE_USER') === 'PRO_USER') {
             // register pro
             $entityManager->beginTransaction();
-            $category = $categoryRep->findById((int) $request->request->get('metier_ask_devis'));
+            $category = $categoryRep->findById((int) $request->request->get('CategoryId'));
             $city = $cityRep->findById((int) $request->request->get('city'));
             $user
                 ->setRoles(['ROLE_USER_PROFESSIONAL'])
